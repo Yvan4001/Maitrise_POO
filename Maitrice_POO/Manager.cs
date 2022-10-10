488ce4ba8cd7644c3cbe05b623b5e4ed346ab77e
Yvan Simon <114400983+Yvan4001@users.noreply.github.com>
2022-10-10 14:38:27 +0200

    Modification des différentes classe pour les rappel + Modification de l'aide

---
+++ Maitrice_POO/Manager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maitrice_POO
+{
+    public class Manager
+    {
+        private static readonly Manager instance = new Manager();
+
+        Manager() { }
+        
+        public static Manager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Permet de rechercher un transport dans un tableau de transport
+        /// </summary>
+        /// <param name="transports"></param>
+        public void RechercheTransport(string[] transports)
+        {
+            Console.Write("Entrer le transport à rechercher: ");
+            var recherche = Console.ReadLine();
+            var trouve = false;
+
+            for (int i = 0; i < transports.Length; i++)
+            {
+                if (transports[i].Equals(recherche))
+                {
+                    trouve = true;
+                }
+                else
+                    trouve = false;
+
+                if (trouve)
+                {
+                    Console.WriteLine("Le transport recherché est: " + transports[i] + " et il est à la position: " + i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Le transport recherché n'existe pas");
+                    break;
+                }
+            }
+
+        }
+
+        public void Permetutation(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        public void DefTableau(out int[] tab, out int[] tab2)
+        {
+            tab = new int[3];
+            tab[0] = 1;
+            tab[1] = 2;
+            tab[2] = 3;
+
+            tab2 = new int[5];
+            tab2[0] = 1;
+            tab2[1] = 2;
+            tab[2] = 3;
+            tab2[3] = 4;
+            tab2[4] = 5;
+        }
+
+        public double Calculer(double a, double b, out double somme, out double moyenne)
+        {
+            somme = a + b;
+            moyenne = (a + b) / 2;
+            return a % b;
+        }
+
+        public double Multiplication(in int a, in int b)
+        {
+            return a * b;
+        }
+
+        public void FonctionSpe(ref int a, ref int b)
+        {
+            PermutationPrivate(ref a, ref b);
+
+            CaluculerPrivate(a, b);
+
+            var multi = Manager.Instance.Multiplication(a, b);
+            Console.WriteLine("Multiplication :" + multi);
+        }
+
+        private void CaluculerPrivate(int a, int b)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Fonction calculer de {a} et {b}");
+            var resultat = Calculer(a, b, out double somme, out double moyenne);
+            Console.WriteLine("Somme: " + somme);
+            Console.WriteLine("Moyenne: " + moyenne);
+            Console.WriteLine("Modulo: " + resultat);
+        }
+
+        private void PermutationPrivate(ref int a, ref int b)
+        {
+            Console.WriteLine($"Permutation de {a} et {b}");
+            Console.WriteLine("Avant permutation: " + a + " et " + b);
+            Permetutation(ref a, ref b);
+            Console.WriteLine("Après permutation: " + a + " et " + b);
+            Console.WriteLine(a);
+        }
+    }
+}
--- Maitrice_POO/Program.cs
+++ Maitrice_POO/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Maitrice_POO;
+class Programm
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        int[] tab, tab2;
+        Manager.Instance.DefTableau(out tab, out tab2);
+
+        Console.WriteLine("Tableau 1");
+        foreach (var item in tab)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Tableau 2: ");
+
+        foreach (var item in tab2)
+        {
+            Console.WriteLine(item);
+        }
+
+        int[,] tab3 = new int[2, 3];
+        tab3[0, 0] = 1;
+        tab3[0, 1] = 2;
+        tab3[0, 2] = 3;
+        tab3[1, 0] = 4;
+        tab3[1, 1] = 5;
+        tab3[1, 2] = 6;
+
+        Console.WriteLine("Tableau 3: ");
+
+        foreach (var item in tab3)
+        {
+            Console.WriteLine(item);
+        }
+
+        int[,,] tab4 = {
+            { { 1, 2, 3 }, { 4, 5, 6 } },
+            { { 7, 8, 9 }, { 10, 11, 12 } }
+        };
+
+        Console.WriteLine("Tableau 4: ");
+        foreach (var item in tab4)
+        {
+            Console.WriteLine(item);
+        }
+        string[] transports = { "Moto", "Voiture", "Avion", "Bateau", "Train", "Bus", "Velo", "Avion" };
+        Manager.Instance.RechercheTransport(transports);
+
+        Console.WriteLine();
+        var a = 10;
+        var b = 30;
+
+        Manager.Instance.FonctionSpe(ref a, ref b);
+    }
+}
